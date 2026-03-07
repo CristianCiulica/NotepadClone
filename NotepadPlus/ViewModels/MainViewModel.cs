c8667de0a774030f2ea131f794665f601ad07eb7
CristianCiulica <cristian.ciulica@student.unitbv.ro>
2026-03-07 13:13:11 +0200

    Add project files.

---
+++ NotepadPlus/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadPlus.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<TabViewModel> Tabs { get; set; }
+        private TabViewModel _selectedTab;
+        public TabViewModel SelectedTab
+        {
+            get { return _selectedTab; }
+            set
+            {
+                _selectedTab = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            Tabs = new ObservableCollection<TabViewModel>();
+            AddNewTab();
+        }
+
+        public void AddNewTab()
+        {
+            int nextFileNumber = Tabs.Count + 1;
+            var newTab = new TabViewModel($"File {nextFileNumber}");
+
+            Tabs.Add(newTab);
+            SelectedTab = newTab;
+        }
+    }
+}
---
+++ NotepadPlus/ViewModels/TabViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotepadPlus.ViewModels
+{
+    public class TabViewModel : ViewModelBase
+    {
+        private string _fileName;
+        private string _textContent;
+        private bool _isSaved;
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set
+            {
+                _fileName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string TextContent
+        {
+            get { return _textContent; }
+            set
+            {
+               _textContent = value; OnPropertyChanged();
+            }
+        }
+        public bool IsSaved
+        {
+            get { return _isSaved; }
+            set { _isSaved = value; OnPropertyChanged(); }
+        }
+        public TabViewModel(string initialName)
+        {
+            FileName = initialName;
+            TextContent = "";
+            IsSaved = false;
+        }
+    }
+}
